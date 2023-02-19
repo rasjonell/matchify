92e9ce732cd8074f4196d39ccc36a33f62af6137
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2023-02-19 17:47:13 +0400

    [App] Add API error handler

--- src/routes/api/auth/login/+server.ts
+++ src/routes/api/auth/login/+server.ts
@@ -1,4 +1,5 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
+
 import { UserModel } from '$lib/server/db/user.server';
 import { SpotifyAPI } from '$lib/server/api/spotify.server';
 import { InterestsModel } from '$lib/server/db/interests.server';
@@ -9,24 +10,30 @@ import type { RequestHandler } from '../$types';
  * handles `POST /api/auth/login`
  */
 export const POST = (async ({ request, cookies }) => {
-	const { code } = await request.json();
-	const tokenData = await SpotifyAPI.getTokens(code);
-	const profile = await SpotifyAPI.getProfileData(tokenData.access);
-	const featureSet = await SpotifyAPI.getTrackFeatures(tokenData.access);
-	const interests = InterestsModel.mergeByAverage(featureSet);
+	try {
+		const { code } = await request.json();
+		const tokenData = await SpotifyAPI.getTokens(code);
+		const profile = await SpotifyAPI.getProfileData(tokenData.access);
+		const featureSet = await SpotifyAPI.getTrackFeatures(tokenData.access);
+		const interests = InterestsModel.mergeByAverage(featureSet);
 
-	let user = await UserModel.getByIdAndUpdate(profile.id, tokenData);
-	if (user) {
-		if (user.interests) {
-			await InterestsModel.update(user.interests, interests);
+		let user = await UserModel.getByIdAndUpdate(profile.id, tokenData);
+		if (user) {
+			if (user.interests) {
+				await InterestsModel.update(user.interests, interests);
+			} else {
+				await InterestsModel.create(user, interests);
+			}
 		} else {
-			await InterestsModel.create(user, interests);
+			user = await UserModel.create(profile, tokenData, interests);
 		}
-	} else {
-		user = await UserModel.create(profile, tokenData, interests);
+
+		cookies.set('session-id', user.id, { path: '/' });
+
+		return json(user);
+	} catch (err) {
+		const asError = err as Error;
+		console.error(asError.cause);
+		throw error(400, asError);
 	}
-
-	cookies.set('session-id', user.id, { path: '/' });
-
-	return json(user);
 }) satisfies RequestHandler;
