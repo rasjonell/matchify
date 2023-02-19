8d996a6526beec5f6c450bb53b54498583dc9440
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2023-02-19 17:11:13 +0400

    [Router] Navigate to user's profile using `user.id`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,11 @@
+const bases = ['progress', 'tooltip'];
+const types = ['info', 'error', 'success', 'warning', 'primary', 'secondary'];
+const safelist = bases.flatMap((base) =>
+	types.map((type) => `${base}-${type}`)
+);
+
 module.exports = {
+	safelist,
 	content: ['./src/{routes,lib}/**/*.{svelte,js,ts}'],
 	plugins: [require('daisyui')],
 };
